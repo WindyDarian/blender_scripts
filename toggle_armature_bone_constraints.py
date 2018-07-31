7ad1491e2f571bb70c7fc0beb748fee70d597f7f
Windy Darian <p123456638@msn.com>
2018-07-31 01:20:13 -0400

    added a script to toggle all bone constraints in an armature named 'armature'; 'metarig->armature'

---
+++ toggle_armature_bone_constraints.py
@@ -0,0 +1,31 @@
+# Copyright 2018 Windy Darian (Ruoyu Fan). MIT License.
+'''
+Toggle all bone constraints in the armature named "armature"
+Blender 2.79b
+'''
+
+import bpy
+
+_armature_name = 'armature'
+'''
+def disable_bone_constraints():
+    obj = bpy.data.objects.get(_armature_name)
+    for bone in obj.pose.bones:
+        for con in bone.constraints:
+            con.mute = True
+
+def enable_bone_constraints():
+    obj = bpy.data.objects.get(_armature_name)
+    for bone in obj.pose.bones:
+        for con in bone.constraints:
+            con.mute = False
+'''
+def toggle_bone_constraints():
+    obj = bpy.data.objects.get(_armature_name)
+    for bone in obj.pose.bones:
+        for con in bone.constraints:
+            con.mute = not con.mute
+
+if __name__ == '__main__':
+    toggle_bone_constraints()
+    print('Job done!')
